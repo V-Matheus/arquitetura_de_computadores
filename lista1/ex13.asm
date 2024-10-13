e1628f7a1dfe8971f33035c823f6f34dc69cefbd
Victor Matheus <victormatheus507@gmail.com>
2024-10-13 15:46:39 -0300

    ex13

---
+++ lista1/ex13.asm
@@ -0,0 +1,51 @@
+.text
+
+main:
+
+	addi $4, $0, 'H'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'e'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'l'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'l'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'o'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, ' '
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'W'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'o'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'r'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'l'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, 'd'
+	add $2, $0, 11
+	syscall
+	
+	addi $4, $0, '!'
+	add $2, $0, 11
+	syscall
