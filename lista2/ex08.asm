ac57939cbb738219f904ddf03d6f919d5a587bdf
Victor Matheus lopes de Sousa <victormatheus507@gmail.com>
2024-11-07 11:43:08 -0300

    ex08 - finalizado

--- lista2/ex08.asm
+++ lista2/ex08.asm
@@ -20,8 +20,47 @@ add $10, $0, $2
 addi $11, $0, 13
 
 slt $12, $10, $0
-beq $12, $0, verfMes
+beq $12, $0, verfAno
 j invalido
+
+verfAno:
+
+# verificar se ano é bissexto
+addi $12, $0, 4
+
+div $10, $12
+mfhi $13
+beq $13, $0, etapa2
+j naoBissexto
+
+etapa2:
+addi $12, $0, 100
+div $10, $12
+mfhi $13
+beq $13, $0, etapa3
+j etapa4
+
+etapa3:
+addi $12, $0, 400
+div $10, $12
+mfhi $13
+beq $13, $0, bissexto
+j etapa4
+
+etapa4:
+addi $12, $0, 4
+
+div $10, $12
+mfhi $13
+beq $13, $0, bissexto
+
+bissexto:
+addi $13, $0, 1
+j verfMes
+
+naoBissexto:
+addi $13, $0, 0
+j verfMes
 
 verfMes:
 slt $12, $0, $9
@@ -42,7 +81,7 @@ beq $9, 11, nov
 beq $9, 12, dez
 
 jan:
-addi $11, $0, 31
+addi $11, $0, 32
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -50,15 +89,23 @@ beq $12, $0, invalido
 j valido
 
 fev:
+beq $13, $0, fevNaoBis
+addi $11, $0, 30
+j verFev
+
+fevNaoBis:
 addi $11, $0, 29
+j verFev
+
+verFev:
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
 beq $12, $0, invalido
-j valido
+j mar
 
 mar:
-addi $11, $0, 31
+addi $11, $0, 32
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -66,7 +113,7 @@ beq $12, $0, invalido
 j valido
 
 abr:
-addi $11, $0, 30
+addi $11, $0, 31
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -74,7 +121,7 @@ beq $12, $0, invalido
 j valido
 
 mai:
-addi $11, $0, 31
+addi $11, $0, 32
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -82,7 +129,7 @@ beq $12, $0, invalido
 j valido
 
 jun:
-addi $11, $0, 30
+addi $11, $0, 31
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -90,7 +137,7 @@ beq $12, $0, invalido
 j valido
 
 jul:
-addi $11, $0, 31
+addi $11, $0, 32
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -98,7 +145,7 @@ beq $12, $0, invalido
 j valido
 
 ago:
-addi $11, $0, 31
+addi $11, $0, 32
 slt $12, $0, $8
 beq $12, $0, invalido
 slt $12, $8, $11
@@ -113,7 +160,15 @@ slt $12, $8, $11
 beq $12, $0, invalido
 j valido
 
-set:
+out:
+addi $11, $0, 32
+slt $12, $0, $8
+beq $12, $0, invalido
+slt $12, $8, $11
+beq $12, $0, invalido
+j valido
+
+nov:
 addi $11, $0, 31
 slt $12, $0, $8
 beq $12, $0, invalido
@@ -121,17 +176,24 @@ slt $12, $8, $11
 beq $12, $0, invalido
 j valido
 
+dez:
+addi $11, $0, 32
+slt $12, $0, $8
+beq $12, $0, invalido
+slt $12, $8, $11
+beq $12, $0, invalido
+j valido
 
 valido:
 addi $4, $0, 1
 addi $2, $0, 1
 syscall
-addi $2, $0, 11
+addi $2, $0, 10
 syscall
 
 invalido:
 addi $4, $0, 0
 addi $2, $0, 1
 syscall
-addi $2, $0, 11
+addi $2, $0, 10
 syscall
